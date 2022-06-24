d53fa77f40ff22814d559b5ed1de5b86934dddb3
LENOVO <mohamed209059@gmail.com>
2022-06-24 17:21:03 +0200

    Initial commit

--- lib/screens/success_register_screen/success_register_screen.dart
+++ lib/screens/success_register_screen/success_register_screen.dart
@@ -20,8 +20,9 @@ class _SuccessRegisterScreenState extends State<SuccessRegisterScreen> {
   void initState() {
     // TODO: implement initState
     super.initState();
-    Timer(Duration(seconds: 5), ()=> Get.to(HomeMainScreen(valueBack: '',)));
+    Timer(Duration(seconds: 3), ()=> Get.to(HomeMainScreen(valueBack: '',)));
   }
+
   @override
   Widget build(BuildContext context) {
     var hight = Get.height *0.024;
