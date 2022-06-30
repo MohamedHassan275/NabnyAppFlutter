879874718efa655cbb45ca9383322ea2895ad765
LENOVO <mohamed209059@gmail.com>
2022-06-30 21:35:37 +0200

    Initial commit

--- lib/core/servies/services.dart
+++ lib/core/servies/services.dart
@@ -2,13 +2,30 @@
 import 'package:get/get.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
-class MyServices extends GetxService {
+abstract class StorageKeys {
+  StorageKeys();
 
-  late SharedPreferences sharedPreferences ;
+  //Declare all storage keys here & create its correpsonding setters & getters
+  static const String token = "TOKEN";
+
+
+}
+
+class MyServices extends GetxService {
+ late SharedPreferences sharedPreferences ;
 
   Future<MyServices> inite() async {
     sharedPreferences = await SharedPreferences.getInstance();
     return this;
+  }
+
+  //TOKEN
+  String get token {
+    return sharedPreferences.getString(StorageKeys.token) ?? "";
+  }
+
+  set token(String token) {
+    sharedPreferences.setString(StorageKeys.token, token);
   }
 
 }
